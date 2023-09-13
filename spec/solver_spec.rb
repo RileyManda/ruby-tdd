828d445d9fc6f699d925efc8fcdd9c2b84e017b2
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-09-13 17:14:30 +0300

    Write tests to check whether the number is negative/positive or 0

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,9 +7,17 @@ describe Solver do
       expect(Solver.new.method(:factorial).arity).to(eq(1))
     end
 
-    it 'returns the correct factorial for poitive integers' do
-      expect(Solver.new.factorial(5)).to(eq(120))
-      expect(Solver.new.factorial(3)).to(eq(6))
+    it 'returns the correct factorial for positive integers' do
+      expect(Solver.new.factorial(5)).to eq 120
+      expect(Solver.new.factorial(3)).to eq 6
     end
+
+    it 'if the number is 0, factorial should be 1' do
+      expect(Solver.new.factorial(0)).to eq 1
+    end
+
+    it 'if the number is negative, it should raise an exception' do
+      expect{Solver.new.factorial(-5)}.to (raise_error(ArgumentError))
   end
+end
 end
