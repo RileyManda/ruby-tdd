0bc363e40d7b0f71413e3eab6d891987d4bbfbcb
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-09-13 17:46:50 +0300

    Fix the order of first condition

--- solver.rb
+++ solver.rb
@@ -13,8 +13,8 @@ class Solver
     word.reverse
   end
 
- def fizzbuzz(number)
-    if (number % 5).zero? && (number % 3).zero?
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
       'fizzbuzz'
     elsif (number % 3).zero?
       'fizz'
